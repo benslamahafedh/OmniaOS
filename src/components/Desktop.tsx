c44c4a0a15f7b11f1e04973993e5ebdd1f131c84
oussamaelkamell <oussamaaelkamel@gmail.com>
2026-06-02 12:36:56 +0100

    fix

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -602,7 +602,7 @@ const Desktop: React.FC = () => {
           </div>
 
           <div className="flex items-center space-x-6 pr-6">
-            {/* Social Media Links - Hide on mobile or show simplified */}
+
             {!isMobile && (
               <div className="flex items-center space-x-3">
 
